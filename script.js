4af9794fa9467c35e9e57565d28284ed16186a18
omieuq <133672864+omieuq@users.noreply.github.com>
2026-08-17 20:03:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 function hideAll() {
-    document.querySelectorAll(".card")
-        .forEach(card => card.classList.remove("active"));
+    document.querySelectorAll(".card").forEach(card => {
+        card.classList.remove("active");
+    });
 }
 
 function showOops() {
-    alert("Кнопка нажалась");
-
     hideAll();
-
-    document.getElementById("screen2")
-        .classList.add("active");
+    document.getElementById("screen2").classList.add("active");
 }
 
 function showLoading() {
-
     hideAll();
+    document.getElementById("screen3").classList.add("active");
 
-    document.getElementById("screen3")
-        .classList.add("active");
-
-    setTimeout(() => {
-
+    setTimeout(function () {
         hideAll();
-
-        document.getElementById("screen4")
-            .classList.add("active");
-
+        document.getElementById("screen4").classList.add("active");
     }, 4000);
 }
