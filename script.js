e6333fc969907109465b3fd870060da18699a957
omieuq <133672864+omieuq@users.noreply.github.com>
2026-08-17 18:40:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hideAll() {
+    document.querySelectorAll(".card")
+        .forEach(card => card.classList.remove("active"));
+}
+
+function showOops() {
+    hideAll();
+    document.getElementById("screen2")
+        .classList.add("active");
+}
+
+function showLoading() {
+
+    hideAll();
+
+    document.getElementById("screen3")
+        .classList.add("active");
+
+    setTimeout(() => {
+
+        hideAll();
+
+        document.getElementById("screen4")
+            .classList.add("active");
+
+    }, 4000);
+}
